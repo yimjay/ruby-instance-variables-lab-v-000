de664ac6a140200221677c8ef886ed318e1273f5
Ji Yim <gsooyim@gmail.com>
2018-04-24 19:53:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def name=(dog_name) #setting name
+    @this_dogs_name = dog_name
+  end
+  
+  def name #getting name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
